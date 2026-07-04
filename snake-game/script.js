63af58658501f7f2e4337b398774d996930842b7
unknown <vermajanhvi95@gmail.com>
2026-07-04 12:39:41 +0530

    Added swipe controls

--- snake-game/script.js
+++ snake-game/script.js
@@ -11,6 +11,10 @@ y:Math.floor(Math.random()*20)*box
 };
 
 let direction="RIGHT";
+let score = 0;
+let highScore = localStorage.getItem("highScore") || 0;
+
+document.getElementById("highScore").innerText = highScore;
 
 document.addEventListener("keydown",changeDirection);
 
@@ -43,6 +47,19 @@ if(direction==="RIGHT")snakeX+=box;
 if(direction==="DOWN")snakeY+=box;
 
 if(snakeX==food.x&&snakeY==food.y){
+    score++;
+document.getElementById("score").innerText = score;
+
+if(score > highScore){
+    highScore = score;
+    localStorage.setItem("highScore", highScore);
+    document.getElementById("highScore").innerText = highScore;
+}
+if (score % 5 === 0) {
+    clearInterval(game);
+    speed = Math.max(50, speed - 10);
+    game = setInterval(draw, speed);
+}
 food={
 x:Math.floor(Math.random()*20)*box,
 y:Math.floor(Math.random()*20)*box
@@ -59,14 +76,15 @@ snakeX>=400||snakeY>=400||
 snake.some(s=>s.x===snakeX&&s.y===snakeY)
 ){
 clearInterval(game);
-alert("Game Over");
+alert("Game Over!\nYour Score:" + score);
 }
 
 snake.unshift(newHead);
 
 }
 
-const game=setInterval(draw,120);
+let speed = 120;
+let game = setInterval(draw, speed);
 document.getElementById("up").addEventListener("click", () => {
   if (direction !== "DOWN") direction = "UP";
 });
@@ -81,4 +99,36 @@ document.getElementById("left").addEventListener("click", () => {
 
 document.getElementById("right").addEventListener("click", () => {
   if (direction !== "LEFT") direction = "RIGHT";
+});
+document.getElementById("restart").addEventListener("click", function () {
+    location.reload();
+});
+let startX = 0;
+let startY = 0;
+
+canvas.addEventListener("touchstart", function(e) {
+    startX = e.touches[0].clientX;
+    startY = e.touches[0].clientY;
+});
+
+canvas.addEventListener("touchend", function(e) {
+    let endX = e.changedTouches[0].clientX;
+    let endY = e.changedTouches[0].clientY;
+
+    let dx = endX - startX;
+    let dy = endY - startY;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+        if (dx > 0 && direction !== "LEFT") {
+            direction = "RIGHT";
+        } else if (dx < 0 && direction !== "RIGHT") {
+            direction = "LEFT";
+        }
+    } else {
+        if (dy > 0 && direction !== "UP") {
+            direction = "DOWN";
+        } else if (dy < 0 && direction !== "DOWN") {
+            direction = "UP";
+        }
+    }
 });
